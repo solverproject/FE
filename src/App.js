24afe744b4032ec27be2e2473a1e2ded797b3ea5
Gnar-dev <initiating94@gmail.com>
2023-03-13 21:15:21 +0900

    chore: 의존성 패키지 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { QueryClient, QueryClientProvider } from "react-query";
 import "./App.css";
+import Router from "./shared/Router";
+import GlobalStyle from "./utils/styles/GlobalStyle";
 
+const queryClient = new QueryClient();
 function App() {
-  return <div className="App"></div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle />
+      <Router />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
