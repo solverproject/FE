e3b2b69b161aec688a487f5391d8c660a5b9d2b5
akfangus <39788340+akfangus@users.noreply.github.com>
2023-03-17 15:35:15 +0900

    Delete api_key.js
    지워버리기

--- src/api/api_key.js
+++
@@ -1,7 +0,0 @@
-//카카오 api key
-const KAKAO_REST_API_KEY = "69e38f21fc5c4ccd6f0a47a084915c4b";
-//네이버 api key
-const NAVER_REST_API_KEY = "4tMMjDn7qTu7L9lLfqz2";
-//구글 api key
-const GOOGLE_REST_API_KEY = "865590021289-hucshpmb3b7pd6u2be8fo7bs1oqsloir";
-export { KAKAO_REST_API_KEY, NAVER_REST_API_KEY, GOOGLE_REST_API_KEY };
